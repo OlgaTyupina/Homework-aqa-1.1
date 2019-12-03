03ba3b2eafee19abda503b753ef8d6126c5d6b52
OlgaTyupina <57899824+OlgaTyupina@users.noreply.github.com>
2019-12-03 22:46:40 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -7,29 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
 @Test
-void cashbackNotChargedPayAddition() {
-        CashbackHackService cashback = new CashbackHackService();
-         int amount = 900;
-         int  payInAddition = boundary - amount;
+void cashbackNotEnough100IfAmount900() {
+     CashbackHackService cashback = new CashbackHackService();
+     int amount = 900;
+         
 
-         int actual = cashback.remain(amount);
-         String expected = "Вам нужно докупить на " + payInAddition;
+      int actual = cashback.remain(amount);
+      int expected = 100;
 
-         assertEquals(expected, actual);
+      assertEquals(expected, actual);
     }
 
 @Test
-void cashbackChargedFrom1000() {
-        CashbackHackService cashback1000 = new CashbackHackService();
-        int amount = 1000;
+void cashbackNotEnough0IfAmount1000() {
+     CashbackHackService cashback1000 = new CashbackHackService();
+     int amount = 1000;
 
-        int actual = cashback1000.remain(amount);
-        String expected = "Вам начислен кэшбэк" ;
+     int actual = cashback1000.remain(amount);
+     int expected = 0;
 
-        assertEquals(expected, actual);
+     assertEquals(expected, actual);
     }
     
-
-
 
 }
