37dc033cbb2525db9b6d74039b698dc39cdd1d66
OlgaTyupina <57899824+OlgaTyupina@users.noreply.github.com>
2019-12-04 15:43:14 +0300

    Update CashbackHackService.java

--- src/main/java/ru/netology/unit/CashbackHackService.java
+++ src/main/java/ru/netology/unit/CashbackHackService.java
@@ -4,7 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        int payInAddition = 0;
         return boundary - amount % boundary;
     }
 }
